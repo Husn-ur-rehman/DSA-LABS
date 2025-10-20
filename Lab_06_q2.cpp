e41dceba2ac5cb3b133c743e4cb4d6d3ed251e69
HUSN UR REHMAN KHAN <khanhusnurrehman@gmail.com>
2025-10-20 14:53:23 +0500

    ""

---
+++ Lab_06_q2.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+    int BubbleSort(vector<int> &test,int &k){
+        for( int i = 0; i < test.size() ; i++ ){
+            for( int j = i; j < test.size(); j++){
+                if(test[i] > test[j]){
+                    swap(test[i],test[j]);
+                }
+            }
+        }
+        int count = 0;
+        for(int i = 0; i < test.size(); i++){
+            if(k <= test[i]) return count;
+            if(k > test[i]){ 
+            k -= test[i];
+            count++;
+            }
+        }
+        return count;
+    }
+};
+
+int main(){
+    vector<int> testing = {1,12,5,111,200,1000,10};
+    int money;
+    cin>>money;
+    Solution obj;
+    cout<<obj.BubbleSort(testing,money)<<endl;
+    cout<<"REMAINING AMOUNT : "<<money;
+}
