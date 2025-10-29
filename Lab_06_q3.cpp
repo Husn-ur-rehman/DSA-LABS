7ab026a8c9f48f3a754b2a5ee1d75cb568ce9723
HUSN UR REHMAN KHAN <khanhusnurrehman@gmail.com>
2025-10-29 09:21:25 +0500

    ""

--- Lab_06_q3.cpp
+++ Lab_06_q3.cpp
@@ -3,33 +3,56 @@ using namespace std;
 
 class Sort{
     public:
-    bool BubbleSort(vector<int> &test){
-        bool flag = false;
-        for( int i = 0; i < test.size() ; i++ ){
-            for( int j = i; j < test.size(); j++){
-                if(test[i] > test[j]){
-                    swap(test[i],test[j]);
-                    flag =true;
-                }
+    void Sorting_Techniques(vector<int> &test){
+        char input;
+        cout<<" WHICH SORTING TECHNIQUE YOU WOULD LIKE TO CHOSE "<<endl<<"A . BUBBLE SORT"<<endl<<"B. INSERTION SORT"<<endl<<"C.SELECTION SORT"<<endl;
+        cin>>input;
+        switch(input){
+            case 'A':{
+                BubbleSort(test);
+                break;
+            }
+            case 'B':{
+                insertionSort(test);
+                break;
+            }
+            case 'C':{
+                SelectionSort(test);
+                break;
             }
         }
-        return flag;
+    }
+    void BubbleSort(vector<int> &test){
+        
+        for( int i = 0; i < test.size() ; i++ ){
+            for( int j = i; j < test.size(); j++){
+                if(test[i] > test[j]) swap(test[i],test[j]);
+            }
+        }
     }
     void insertionSort(vector<int>& arr) {
-    int n = arr.size();
-    for (int i = 1; i < n; i++) {
-        int key = arr[i];        // current element to insert
-        int j = i - 1;
+        int n = arr.size();
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
 
-        // shift elements to the right until correct spot found
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j--;
+            arr[j + 1] = key;
         }
-
-        arr[j + 1] = key;        // insert key at correct position
     }
-}    
+    void SelectionSort(vector<int> &test){
+        int n = test.size();
+        for(int i = 0; i < n-1; i++){
+            int min__ind = i;
+            for(int j = i; j < n; j++){
+                if(test[j] < test[min__ind]) min__ind = j;
+            }
+            swap(test[i],test[min__ind]);
+        }
+    }    
     void display(vector<int> test){
         for(int i = 0; i < test.size();i++)
         cout<<test[i]<<" ";
@@ -38,6 +61,7 @@ class Sort{
 int main(){
     Sort obj;
     vector<int> testing = {23,14,214,432,54,21,};
-    obj.insertionSort(testing);
+    obj.Sorting_Techniques(testing);
     obj.display(testing);
+
 }
